fadc411391785fd77504f8716f292b8086f5e5c1
kt25-86 <119519382+kt25-86@users.noreply.github.com>
2026-04-16 14:31:47 -0700

    Update vm.h

--- CH15/vm.h
+++ CH15/vm.h
@@ -19,26 +19,10 @@ typedef enum {
   INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
-void initVM(){
-  vm.stackCapacity = 256;
-  vm.stack = malloc(sizeof(Value) * vm.stackCapacity);
-  resetStack();
-}
-
-void freeVM(){
-  free(vm.stack);
-}
-
+void initVM();
+void freeVM();
 InterpretResult interpret(Chunk* chunk);
-void push(Value value){
-  if(vm.stackTop - vm.stack >= vm.stackCapacity){
-    vm.stackCapacity *= 2;
-    vm.stack = realloc(vm.stack, sizeof(Value) * vm.stackCapacity);
-    vm.stackTop = vm.stack + vm.stackCapacity / 2;
-  }
-  *vm.stackTop = value;
-  vm.stackTop++;
-}
+void push(Value value);
 Value pop();
 
 #endif
