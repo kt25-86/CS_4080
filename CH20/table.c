bfd10f3b39dbf01a49da07ce0ed434d94deb537f
kt25-86 <119519382+kt25-86@users.noreply.github.com>
2026-04-16 15:08:12 -0700

    Create table.c

---
+++ CH20/table.c
@@ -0,0 +1,150 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "table.h"
+#include "value.h"
+
+#define TABLE_MAX_LOAD 0.75
+
+void initTable(Table* table) {
+  table->count = 0;
+  table->capacity = 0;
+  table->entries = NULL;
+}
+
+void freeTable(Table* table) {
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  initTable(table);
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+
+   if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+    int capacity = GROW_CAPACITY(table->capacity);
+    adjustCapacity(table, capacity);
+  }
+  
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  bool isNewKey = entry->key == NULL;
+  if (isNewKey && IS_NIL(entry->value)) table->count++;
+
+  entry->key = key;
+  entry->value = value;
+  return isNewKey;
+}
+
+static Entry* findEntry(Entry* entries, int capacity, Value key) {
+  uint32_t index = hashValue(key) % capacity;
+  Entry* tombstone = NULL;
+  for (;;) {
+    Entry* entry = &entries[index];
+   if (entry->key == NULL) {
+      if (IS_NIL(entry->value)) {
+        // Empty entry.
+        return tombstone != NULL ? tombstone : entry;
+      } else {
+        // We found a tombstone.
+        if (tombstone == NULL) tombstone = entry;
+      }
+    } else if (valuesEqual(entry->key, key)) {
+      // We found the key.
+      return entry;
+    }
+    index = (index + 1) % capacity;
+  }
+}
+static void adjustCapacity(Table* table, int capacity) {
+  Entry* entries = ALLOCATE(Entry, capacity);
+  for (int i = 0; i < capacity; i++) {
+    entries[i].key = NIL_VAL;
+    entries[i].value = NIL_VAL;
+  }
+  table->count = 0;
+  for (int i = 0; i < table->capacity; i++) {
+    Entry* entry = &table->entries[i];
+    if (entry->key == NULL) continue;
+
+    Entry* dest = findEntry(entries, capacity, entry->key);
+    dest->key = entry->key;
+    dest->value = entry->value;
+    table->count++;
+  }
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  table->entries = entries;
+  table->capacity = capacity;
+}
+
+void tableAddAll(Table* from, Table* to) {
+  for (int i = 0; i < from->capacity; i++) {
+    Entry* entry = &from->entries[i];
+    if (entry->key != NULL) {
+      tableSet(to, entry->key, entry->value);
+    }
+  }
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+  if (table->count == 0) return false;
+
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+
+  *value = entry->value;
+  return true;
+}
+
+bool tableDelete(Table* table, Value key) {
+  if (table->count == 0) return false;
+
+  // Find the entry.
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+
+  // Place a tombstone in the entry.
+  entry->key = NIL_VAL;
+  entry->value = BOOL_VAL(true);
+  return true;
+}
+
+ObjString* tableFindString(Table* table, const char* chars,
+                           int length, uint32_t hash) {
+  if (table->count == 0) return NULL;
+
+  uint32_t index = hash % table->capacity;
+  for (;;) {
+    Entry* entry = &table->entries[index];
+    if (entry->key == NULL) {
+      // Stop if we find an empty non-tombstone entry.
+      if (IS_NIL(entry->value)) return NULL;
+    } else if (entry->key->length == length &&
+        entry->key->hash == hash &&
+        memcmp(entry->key->chars, chars, length) == 0) {
+      // We found it.
+      return entry->key;
+    }
+
+    index = (index + 1) % table->capacity;
+  }
+}
+
+static uint8_t hashValue(Value value){
+  switch(value.type){
+    case VAL_BOOL:
+      return AS_BOOL(value) ? 1 : 0;
+    case VAL_NIL:
+      return 2;
+    case VAL_NUMBER:{
+      double num = AS_NUMBER(value);
+      uint64_t bits;
+      memcpy(&bits,&num,sizeof(bits));
+      return (uint32_t)(bits ^ (bits >> 32));
+    }
+    case VAL_OBJ:
+      if(IS_STRING(value)) return AS_STRING(value)->hash;
+      return 0;
+  }
+  return 0;
+}
+
