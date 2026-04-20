106a4979eee13dedd1ab9854e5da9d79371b6b20
kt25-86 <119519382+kt25-86@users.noreply.github.com>
2026-04-19 19:57:47 -0700

    Create LoxFunction.java

---
+++ CH12/LoxFunction.java
@@ -0,0 +1,61 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+class LoxFunction implements LoxCallable {
+  private final Stmt.Function declaration;
+  private final Environment closure;
+  private final boolean isInitializer;
+
+  LoxFunction(Stmt.Function declaration, Environment closure, boolean isInitializer) {
+    this.isInitializer = isInitializer;
+    this.closure = closure;
+    this.declaration = declaration;
+  }
+
+  LoxFunction bind(LoxInstance instance) {
+    Environment environment = new Environment(closure);
+    environment.define("this", instance);
+    return new LoxFunction(declaration, environment,
+                           isInitializer);
+  }
+
+  public boolean isGetter(){ //challenge 2 ch12
+    return decleration.params == null;
+  }
+  
+  @Override
+  public String toString() {
+    return "<fn " + declaration.name.lexeme + ">";
+  }
+
+  @Override
+  public int arity() {
+    return declaration.params.size();
+  }
+
+  @Override
+  public Object call(Interpreter interpreter, List<Object> arguments) {
+    Environment environment = new Environment(closure);
+
+    if (decleration.params != null) { //challenge 2 ch12
+      for (int i = 0; i < declaration.params.size(); i++) {
+        environment.define(declaration.params.get(i).lexeme,
+            arguments.get(i));
+      }
+    }
+
+    try {
+      interpreter.executeBlock(declaration.body, environment);
+    } catch (Return returnValue) {
+      if (isInitializer) return closure.getAt(0, "this");
+      
+      return returnValue.value;
+    }
+
+    if (isInitializer) return closure.getAt(0, "this");
+    return null;
+  }
+
+  
+}
