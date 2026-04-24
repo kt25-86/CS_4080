51657d5d82cfc97bc0aedf7bd022bf3187a329ee
kt25-86 <119519382+kt25-86@users.noreply.github.com>
2026-04-23 21:20:30 -0700

    Update compiler.c

--- CH23/compiler.c
+++ CH23/compiler.c
@@ -565,6 +565,65 @@ static void whileStatement() {
   emitByte(OP_POP);
 }
 
+static void switchStatement(){  //challenge 1 ch 23
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'switch'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after value.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before switch cases.");
+
+  int state = 0;
+  int caseEnds[MAX_CASES];
+  int caseCount = 0;
+  int previousCaseSkip = -1;
+
+  while (!match(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    if (match(TOKEN_CASE) || match(TOKEN_DEFAULT)) {
+      TokenType case Type = parser.previous.type;
+
+      if (state == 2) {
+        error("Can't have another case of default after the defaul case.");
+      }
+
+      if (state == 1) {
+        caseEnds[caseCount++] = emitJump(OP_JUMP);
+        patchJump(previousCaseSkip);
+        emitByte(OP_POP);
+      }
+
+      if (caseType == TOKEN_CASE){
+        state = 1;
+        emitByte(OP_DUP);
+        expression();
+        consue(TOKEN_COLON, "Expect ':' after case value.");
+        emitByte(OP_EQUAL):
+        previousCaseSkip = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);
+      } else {
+        state = 2;
+        consume(TOKEN_COLON, "Expect ':' after default");
+        previousCaseSkip = -1;
+      }
+    } else {
+      if (state == 0) {
+        error("Can't have statements before any case.");
+      }
+      statement();
+    }
+  }
+
+  if (state == 1){
+    patchJump(previousCaseSkip);
+    emitBytes(OP_POP);
+  }
+
+  for (int i = 0; i < caseCount; i++){
+    patchJump(caseEnds[i]));
+  }
+
+  emitByte(OP_POP);
+               
+}
+
 static void synchronize() {
   parser.panicMode = false;
 
